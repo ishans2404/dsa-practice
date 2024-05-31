6cb9e6aeaa9198cab93a679ddd62efa10b210de7
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-05-31 22:09:39 +0530

    Time: 5 ms (49.27%), Space: 9.1 MB (55.44%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -5,7 +5,7 @@ public:
         cin.tie(nullptr);
         cout.tie(nullptr);
 
-        vector<int> dp(n+1, -1);
+        vector<int> dp(n+1);
         dp[0] = 0;
         for(int i=1; i<=n; i++) {
             if(i%2 == 0) dp[i] = dp[i/2];
