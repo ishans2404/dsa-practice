fdf3b5658998a9f2f906b00e56d6822c5382f218
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-05-31 17:37:24 +0530

    Time: 6 ms (21.52%), Space: 11 MB (28.87%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        int m = *max_element(candies.begin(), candies.end());
+        vector<bool> v;
+        for(int& it:candies) {
+            if(extraCandies + it >= m) v.push_back(true);
+            else v.push_back(false);
+        }
+        return v;
+    }
+};
