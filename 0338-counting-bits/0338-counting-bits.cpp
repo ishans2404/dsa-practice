8f27307cec65622204edb154d2c795a779d1458c
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-05-31 22:17:58 +0530

    Time: 0 ms (100.00%), Space: 9 MB (55.44%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+
         vector<int> dp(n+1, 0);
         for(int i=1; i<=n; i++) {
-            if(i%2 == 0) {
-                dp[i] = dp[i/2];
-            }
-            else {
-                dp[i] = dp[i/2] + 1;
-            }
+            dp[i] = (i%2 == 0) ? (dp[i/2]) : (1 + dp[i/2]);
         }
-        return dp;   
+        return dp;
     }
 };
