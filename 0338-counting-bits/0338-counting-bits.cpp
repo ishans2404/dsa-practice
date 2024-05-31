59080c520d6d2a87d1cf59decf8b8cdb42e46b42
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-05-31 22:11:41 +0530

    Time: 5 ms (49.27%), Space: 9.1 MB (44.44%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -8,8 +8,7 @@ public:
         vector<int> dp(n+1);
         dp[0] = 0;
         for(int i=1; i<=n; i++) {
-            if(i%2 == 0) dp[i] = dp[i/2];
-            else dp[i] = 1 + dp[i/2];
+            dp[i] = (i%2 == 0) ? (dp[i/2]) : (1 + dp[i/2]);
         }
         return dp;
     }
