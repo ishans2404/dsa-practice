d6ed06ea395919d928d4acdb2ca6e2d2176f972b
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-06-01 16:47:21 +0530

    Time: 4 ms (99.93%), Space: 11.3 MB (21.40%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int isVowel(char c) {
-        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') return 1;
-        return 0;
-    }
     int maxVowels(string s, int k) {
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
         cout.tie(nullptr);
-
+        
+        bitset<122> alph;
+        alph.set(97);
+        alph.set(101);
+        alph.set(105);
+        alph.set(111);
+        alph.set(117);
         int vowel = 0, n = s.length();
-        for(int i=0; i<k; i++) vowel += isVowel(s[i]);
+        for(int i=0; i<k; i++) vowel += alph[s[i]];
         int maxVowel = vowel;
         for(int i=k; i<n; i++) {
-            vowel += isVowel(s[i]) - isVowel(s[i-k]);
+            vowel += alph[s[i]] - alph[s[i-k]];
             maxVowel = max(maxVowel, vowel);
         }
         return maxVowel;
