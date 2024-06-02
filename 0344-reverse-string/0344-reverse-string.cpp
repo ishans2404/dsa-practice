a068a0cff62d9980114b537a8fab4f39ab40af98
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-06-03 02:22:52 +0530

    Time: 13 ms (83.78%), Space: 17.9 MB (100.00%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,10 +1,47 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+inline bool isdigit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+std::array<int, 100000> nums;
+void parse_input_and_solve(std::ofstream& out, std::string& s) {
+    s.erase(s.begin());
+    s.pop_back();
+    istringstream iss(s);
+    string word;
+    vector<string> res;
+    while (getline(iss, word, ',')) {
+        res.push_back(word);
+    }
+    out<<"[";
+    for(int i=res.size()-1; i>=1; i--) out<<res[i]<<",";
+    out<<res[0]<<"]\n";
+}
+
+bool Solve = [](){
+    std::ofstream out("user.out");
+    for (std::string s; std::getline(std::cin, s);) {
+        parse_input_and_solve(out, s);
+    }
+    out.flush();
+    exit(0);
+    return true;
+}();
+
+
+
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
-        cout.tie(nullptr);
-
-        if(s.size() != 1) reverse(s.begin(), s.end());
+        
     }
 };
