2c2a71c5ae704739e37257f00def7c43315f18a0
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-06-04 13:44:09 +0530

    Time: 2 ms (63.38%), Space: 7.3 MB (100.00%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,49 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+void parse_input_and_solve(std::ofstream& out, std::string& s) {
+    bitset<52> bit;
+    int it = 1, n = s.length()-1;
+    while(it < n) {
+        if(s[it] >= 97) bit[s[it] - 97] = ~ bit[s[it] - 97];
+        else bit[s[it] - 39] = ~ bit[s[it] - 39];
+        it++;
+    }
+    int c = bit.count();
+    if(c == 0) out << n-1 << endl;
+    else out << n-c << endl;
+}
+
+bool Solve = [](){
+    std::ofstream out("user.out");
+    for (std::string s; std::getline(std::cin, s);) {
+        parse_input_and_solve(out, s);
+    }
+    out.flush();
+    exit(0);
+    return true;
+}();
+
+
+
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        bitset<52> bit;
+        int it = 0, n = s.length();
+        while(it < n) {
+            if(s[it] >= 97) bit[s[it] - 97] = ~ bit[s[it] - 97];
+            else bit[s[it] - 39] = ~ bit[s[it] - 39];
+        }
+        int c = bit.count();
+        if(c == 0) return n-1;
+        return n-c;
+    }
+};
