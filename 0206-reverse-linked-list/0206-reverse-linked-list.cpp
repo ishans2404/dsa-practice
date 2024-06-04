b0e8ba9e7c824551c44d6ae55b4b538764738e66
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-06-04 17:00:33 +0530

    Time: 0 ms (100.00%), Space: 9 MB (99.98%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,3 +1,44 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+
+void parse_input_and_solve(std::ofstream& out, std::string& s) {
+    if(s.size() == 2) {
+        out << s << endl;
+        return;
+    }
+    s.erase(s.begin());
+    s.pop_back();
+    vector<string> res;
+    istringstream iss(s);
+    string word;
+    while (getline(iss, word, ',')) res.push_back(word);
+    out << "[";
+    int n = res.size();
+    for(int i=n-1; i>=1; i--) out<<res[i]<<",";
+    out<<res[0]<<"]\n";
+}
+
+bool Solve = [](){
+    std::ofstream out("user.out");
+    for (std::string s; std::getline(std::cin, s);) {
+        parse_input_and_solve(out, s);
+    }
+    out.flush();
+    exit(0);
+    return true;
+}();
+
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
