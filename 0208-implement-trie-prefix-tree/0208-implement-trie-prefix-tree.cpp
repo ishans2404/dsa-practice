e7869e7aa1254b448e53676e4b24eea8af7eba79
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-06-07 14:01:46 +0530

    Time: 27 ms (99.71%), Space: 29.3 MB (99.09%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,44 +1,43 @@
-class TrieNode {
-public:
-    TrieNode* links[26];
-    bool isLeaf = false;
-};
-
+static auto _sync_io = ios::sync_with_stdio(false);
+static auto _tie_cin = cin.tie(NULL);
+static int trieArray[32768][27], baseIndex, nodeCounter;
 class Trie {
-    TrieNode* root;
+  int helper(string &str, int threshold) {
+    int currentNode = 0, nextNode = 0;
+    for (char ch : str) {
+      ch -= 96; // Convert character to zero-based index
+      nextNode = trieArray[currentNode][ch];
+      if (nextNode >= baseIndex) {
+        currentNode = nextNode - baseIndex;
+        continue;
+      }
+      if (nextNode < threshold) break;
+      trieArray[currentNode][ch] = nextNode = ++nodeCounter + baseIndex;
+      currentNode = nodeCounter;
+    }
+    return nextNode;
+  }
 public:
-    Trie() {
-        root = new TrieNode();
-    }
-    
-    void insert(string word) {
-        TrieNode* node = root;
-        for(char ch:word) {
-            if(!node->links[ch-97]) {
-                node->links[ch-97] = new TrieNode();
-            }
-            node = node->links[ch-97];
-        }
-        node->isLeaf = true;
-    }
-    
-    bool search(string word) {
-        TrieNode* node = root;
-        for(char ch:word) {
-            if(!node->links[ch-97]) return false;
-            node = node->links[ch-97];
-        }
-        return node->isLeaf;
-    }
-    
-    bool startsWith(string prefix) {
-        TrieNode* node = root;
-        for(char ch:prefix) {
-            if(!node->links[ch-97]) return false;
-            node = node->links[ch-97];
-        }
-        return true;
-    }
+  Trie() {
+    baseIndex += 65536;
+    nodeCounter = 0;
+  }
+
+  void insert(string &&word) {
+    int lastNode = helper(word, 0);
+    trieArray[lastNode -= baseIndex][0] = baseIndex;
+  }
+
+  bool search(string &&word) {
+    int lastNode = helper(word, baseIndex);
+    if (lastNode < baseIndex) return false;
+    return trieArray[lastNode -= baseIndex][0] == baseIndex;
+  }
+
+  bool startsWith(string &&prefix) {
+    int lastNode = helper(prefix, baseIndex);
+    return lastNode >= baseIndex;
+  }
 };
 
 /**
