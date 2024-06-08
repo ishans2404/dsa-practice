0c8001ab025d0f1f639f755ba417f16230bd13ba
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-06-08 22:02:59 +0530

    Time: 126 ms (98.23%), Space: 51.8 MB (100.00%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,3 +1,64 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+void parse_input_and_solve(std::ofstream& out, std::string& s, std::string& t) {
+    s.erase(s.begin());
+    s.pop_back();
+    vector<int> nums;
+    istringstream iss(s);
+    string word;
+    while(getline(iss, word, ',')) {
+        nums.push_back(stoi(word));
+    }
+    
+    bool flag = false;
+    int k = stoi(t);
+    int n = nums.size();
+    unordered_map<int, int> map;
+    map[0] = -1;
+    int cumsum = 0;
+
+    for(int i=0; i<n; i++) {
+        cumsum += nums[i];
+        int rem = cumsum % k;
+
+        if(map.find(rem) == map.end()) { // map[rem] not present
+            map[rem] = i;
+        }
+        else {
+            if(i - map[rem] > 1) {
+                flag = true;
+                goto label;
+            }
+        }
+    }
+    
+    label:
+    if(flag) out<<"true"<<endl;
+    else out<<"false"<<endl;
+}
+
+bool Solve = []() {
+    std::ofstream out("user.out");
+    std::string s, t;
+    while (std::getline(std::cin, s) && std::getline(std::cin, t)) {
+        parse_input_and_solve(out, s, t);
+    }
+    out.flush();
+    exit(0);
+    return true;
+}();
+
+
+
+
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
