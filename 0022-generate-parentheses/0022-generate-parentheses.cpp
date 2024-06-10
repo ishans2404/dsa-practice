c6d26a73a97dbe3c3df77ec2f4c068eddd806dc8
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-06-10 20:32:29 +0530

    Time: 3 ms (63.20%), Space: 9 MB (98.81%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,50 +1,50 @@
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 
-static const bool Booster = [](){
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return true;
-}();
+// static const bool Booster = [](){
+//     std::ios_base::sync_with_stdio(false);
+//     std::cin.tie(nullptr);
+//     std::cout.tie(nullptr);
+//     return true;
+// }();
 
-void parse_input_and_solve(std::ofstream& out, const std::string& s) {
-    int n = stoi(s);
-    vector<vector<string>> dp(n + 1);
-    dp[0].push_back("");
+// void parse_input_and_solve(std::ofstream& out, const std::string& s) {
+//     int n = stoi(s);
+//     vector<vector<string>> dp(n + 1);
+//     dp[0].push_back("");
 
-    for(int i = 1; i <= n; ++i) {
-        for(int j = 0; j < i; ++j) {
-            for(string& x : dp[j]) {
-                for(string& y : dp[i - j - 1]) {
-                    dp[i].push_back("(" + x + ")" + y);
-                }
-            }
-        }
-    }
+//     for(int i = 1; i <= n; ++i) {
+//         for(int j = 0; j < i; ++j) {
+//             for(string& x : dp[j]) {
+//                 for(string& y : dp[i - j - 1]) {
+//                     dp[i].push_back("(" + x + ")" + y);
+//                 }
+//             }
+//         }
+//     }
     
-    out << "[";
-    int k = dp[n].size();
-    for(int i = 0; i < k-1; i++) {
-        out << "\"";
-        out << dp[n][i];
-        out << "\"";
-        out << ",";
-    }
-    out << "\"";
-    out << dp[n][k-1];
-    out << "\"]" << endl;
-}
+//     out << "[";
+//     int k = dp[n].size();
+//     for(int i = 0; i < k-1; i++) {
+//         out << "\"";
+//         out << dp[n][i];
+//         out << "\"";
+//         out << ",";
+//     }
+//     out << "\"";
+//     out << dp[n][k-1];
+//     out << "\"]" << endl;
+// }
 
-bool Solve = [](){
-    std::ofstream out("user.out");
-    for (std::string s; std::getline(std::cin, s);) {
-        parse_input_and_solve(out, s);
-    }
-    out.flush();
-    exit(0);
-    return true;
-}();
+// bool Solve = [](){
+//     std::ofstream out("user.out");
+//     for (std::string s; std::getline(std::cin, s);) {
+//         parse_input_and_solve(out, s);
+//     }
+//     out.flush();
+//     exit(0);
+//     return true;
+// }();
 
 
 
