07bf79afb4adcb7e67c6647634da8f843ab87b03
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-06-12 19:10:34 +0530

    Time: 0 ms (100.00%), Space: 7 MB (100.00%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,3 +1,46 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+void parse_input_and_solve(std::ofstream& out, std::string& s) {
+    s.erase(s.begin());
+    s.pop_back();
+    int l = 0, r = s.length()-1, it = 0;
+    while(it <= r) {
+        if(s[it] == '0') {
+            s[it] = s[l];
+            s[l] = '0';
+            it += 2;
+            l += 2;
+        }
+        else if(s[it] == '2') {
+            s[it] = s[r];
+            s[r] = '2';
+            r -= 2;
+        }
+        else it += 2;
+    }
+    out<<"["<<s<<"]\n";
+}
+
+bool Solve = [](){
+    std::ofstream out("user.out");
+    for (std::string s; std::getline(std::cin, s);) {
+        parse_input_and_solve(out, s);
+    }
+    out.flush();
+    exit(0);
+    return true;
+}();
+
+
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
