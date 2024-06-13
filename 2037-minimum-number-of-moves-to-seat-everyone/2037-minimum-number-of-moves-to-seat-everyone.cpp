32bda708925fc67fc2cbe1ed6f3a8a86b042b165
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-06-13 13:10:23 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (100.00%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,3 +1,50 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+void parse_input_and_solve(std::ofstream& out, std::string& s, std::string& t) {
+    s.erase(s.begin());
+    s.pop_back();
+    t.erase(t.begin());
+    t.pop_back();
+    
+    vector<int> v1, v2;
+    string w;
+    
+    istringstream iss1(s);
+    while(getline(iss1, w, ',')) v1.push_back(stoi(w));
+    
+    istringstream iss2(t);
+    while(getline(iss2, w, ',')) v2.push_back(stoi(w));
+    
+    sort(v1.begin(), v1.end());
+    sort(v2.begin(), v2.end());
+    
+    int c = 0, n = v1.size();
+    for(int i=0; i<n; i++) c += abs(v1[i] - v2[i]);
+    
+    out << c << endl;
+}
+
+bool Solve = []() {
+    std::ofstream out("user.out");
+    std::string s, t;
+    while (std::getline(std::cin, s) && std::getline(std::cin, t)) {
+        parse_input_and_solve(out, s, t);
+    }
+    out.flush();
+    exit(0);
+    return true;
+}();
+
+
+
 class Solution {
 public:
     vector<int> countSort(vector<int>& inputArray) {
