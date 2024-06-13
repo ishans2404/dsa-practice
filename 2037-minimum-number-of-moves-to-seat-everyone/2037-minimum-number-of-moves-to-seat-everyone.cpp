06b79d3077bbbb035f27abacb3627696f681e17b
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-06-13 13:04:48 +0530

    Time: 10 ms (22.69%), Space: 21.3 MB (17.35%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,8 +1,24 @@
 class Solution {
 public:
+    vector<int> countSort(vector<int>& inputArray) {
+        int N = inputArray.size();
+        int M = *max_element(inputArray.begin(), inputArray.end());
+
+        vector<int> countArray(M + 1, 0);
+        for (int i = 0; i < N; i++) countArray[inputArray[i]]++;
+        for (int i = 1; i <= M; i++) countArray[i] += countArray[i - 1];
+
+        vector<int> outputArray(N);
+        for (int i = N - 1; i >= 0; i--) {
+            outputArray[countArray[inputArray[i]] - 1] = inputArray[i];
+            countArray[inputArray[i]]--;
+        }
+        return outputArray;
+    }
+
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
-        sort(seats.begin(), seats.end());
-        sort(students.begin(), students.end());
+        seats = countSort(seats);
+        students = countSort(students);
         int c = 0, n = seats.size();
         for(int i=0; i<n; i++) c += abs(seats[i] - students[i]);
         return c;
