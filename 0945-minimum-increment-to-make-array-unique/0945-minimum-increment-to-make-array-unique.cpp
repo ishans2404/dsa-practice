737a1536c212eac2a73c67d489fc734d3885f581
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-06-14 19:08:35 +0530

    Time: 83 ms (97.24%), Space: 73.8 MB (16.34%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        bitset<110000> b;
-        int n = nums.size(), c = 0;
-        for(int i=0; i<n; i++) {
-            int val = nums[i];
-            if(!b[val]) b.set(val);
-            else {
-                while(b[val]) {
-                    val++;
-                    c++;
-                }
-                b.set(val);
-            }
+        auto [minimum, maximum] = minmax_element(nums.begin(), nums.end());
+        int n = nums.size(), min = *minimum, max = *maximum;
+        vector<int> freq(max-min + n+1, 0);
+        for(int i : nums) freq[i-min]++;
+        int count = 0, inc = 0;
+        for(int i=0; count < n; i++) {
+            int f = freq[i];
+            count += (f != 0);
+            if(f <= 1) continue;
+
+            freq[i+1] += (f-1);
+            inc += f-1;
         }
-        return c;
+        return inc;
     }
 };
