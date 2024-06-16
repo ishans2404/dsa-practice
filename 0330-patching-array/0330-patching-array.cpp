54ae7e0793cd124d49797cce651f7c3d45931422
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-06-16 11:31:09 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (100.00%) - LeetHub

--- 0330-patching-array/0330-patching-array.cpp
+++ 0330-patching-array/0330-patching-array.cpp
@@ -1,3 +1,51 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+void parse_input_and_solve(std::ofstream& out, std::string& s, std::string& t) {
+    s.erase(s.begin());
+    s.pop_back();
+    int n = stoi(t);
+    vector<int> nums;
+    string w;
+    istringstream iss(s);
+    while(getline(iss, w, ',')) nums.push_back(stoi(w));
+
+    long long missing = 1;
+    int patch = 0;
+    int idx = 0;
+    int k = nums.size();
+    while(missing <= n) {
+        if(idx < k && nums[idx] <= missing) {
+            missing += nums[idx++];
+        }
+        else {
+            missing *= 2;
+            patch++;
+        }
+    }
+    out << patch << endl;
+}
+
+bool Solve = []() {
+    std::ofstream out("user.out");
+    std::string s, t;
+    while (std::getline(std::cin, s) && std::getline(std::cin, t)) {
+        parse_input_and_solve(out, s, t);
+    }
+    out.flush();
+    exit(0);
+    return true;
+}();
+
+
+
 class Solution {
 public:
     int minPatches(vector<int>& nums, int n) {
