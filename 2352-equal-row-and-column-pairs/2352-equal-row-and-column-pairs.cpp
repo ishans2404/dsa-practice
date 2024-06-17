9b1903f91d4cd1e3271692b6569cf376a7ed44e1
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-06-18 01:49:37 +0530

    Time: 96 ms (50.36%), Space: 30.1 MB (71.28%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
+        unordered_map<string,int> row;
+        string temp;
         int n = grid.size();
-        vector<string> row(n), col(n);
-        string rstr, cstr;
-        for(int i=0; i<n; i++) {
-            cstr = "";
-            rstr = "";
-            for(int j=0; j<n; j++) {
-                rstr += to_string(grid[i][j]) + " ";
-                cstr += to_string(grid[j][i]) + " ";
+        for(int i=0; i<n; i++){
+            temp = "";
+            for(int j=0; j<n; j++){
+                temp += to_string(grid[i][j]) + " ";
             }
-            row[i] = rstr;
-            col[i] = cstr;
+            row[temp]++;
         }
-        int c = 0;
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                if(row[i].length() == col[j].length()) {
-                    if(row[i].compare(col[j]) == 0) c++;
-                }
+        int ans=0;
+        for(int i=0; i<n; ++i){
+            temp="";
+            for(int j=0; j<n; j++){
+                temp += to_string(grid[j][i]) + " ";
             }
+            if(row[temp]) ans += row[temp];
         }
-        return c;
+        return ans;
     }
 };
