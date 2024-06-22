0ea35b9d45ee982a2864aac7b41c763b4a3bc19c
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-06-22 13:17:54 +0530

    Time: 18 ms (82.41%), Space: 11.5 MB (100.00%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,3 +1,49 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+int f(vector<int>& nums, int goal) {
+    int l = 0, r = 0, n = nums.size(), sum = 0, c = 0;
+    while(r < n) {
+        sum += nums[r];
+        while(l <= r && sum > goal) {
+            sum -= nums[l++];
+        }
+        c += r++ - l;
+    }
+    return c;
+}
+
+void parse_input_and_solve(std::ofstream& out, std::string& s, std::string& t) {
+    s.erase(s.begin());
+    s.pop_back();
+    int goal = stoi(t);
+    vector<int> nums;
+    string w;
+    istringstream iss(s);
+    while(getline(iss, w, ',')) nums.push_back(stoi(w));
+    out << f(nums, goal) - f(nums, goal-1) << endl;
+}
+
+bool Solve = []() {
+    std::ofstream out("user.out");
+    std::string s, t;
+    while (std::getline(std::cin, s) && std::getline(std::cin, t)) {
+        parse_input_and_solve(out, s, t);
+    }
+    out.flush();
+    exit(0);
+    return true;
+}();
+
+
+
 class Solution {
 public:
     int function(vector<int>& nums, int goal) {
