0e22680d21f97a088613088daf7e5978ea3a0d8f
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-06-27 14:19:00 +0530

    Time: 0 ms (100.00%), Space: 11 MB (100.00%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,3 +1,48 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+void parse_input_and_solve(std::ofstream& out, std::string& s) {
+    vector<string> v;
+    string t = "";
+    int br = 0;
+    for(char ch : s) {
+        if(ch == '[') continue;
+        else if(isdigit(ch)) t += ch;
+        else if(ch == ']') {
+            br++;
+            if(br == 2) {
+                v.push_back(t);
+                t = "";
+                break;
+            }
+        }
+        else if(ch == ',') {
+            v.push_back(t);
+            t = "";
+        }
+    }
+    if(v[0] == v[2] || v[0] == v[3]) out<<v[0]<<endl;
+    else out<<v[1]<<endl;
+}
+
+bool Solve = [](){
+    std::ofstream out("user.out");
+    for (std::string s; std::getline(std::cin, s);) {
+        parse_input_and_solve(out, s);
+    }
+    out.flush();
+    exit(0);
+    return true;
+}();
+
+
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
