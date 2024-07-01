3ff6c7929504a1b346b8d000222311589392a739
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-07-01 07:37:11 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (100.00%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -1,3 +1,50 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+void parse_input_and_solve(std::ofstream& out, std::string& s) {
+    s.erase(s.begin());
+    s.pop_back();
+    vector<int> arr;
+    string w;
+    istringstream iss(s);
+    
+    bool flag = false;
+    int odd = 0;
+    while(getline(iss, w, ',')) {
+        if(w.back() & 1 == 1) {
+            odd++;
+            if(odd == 3) {
+                flag = true;
+                goto label;
+            }
+        }
+        else odd = 0;
+    }
+    label:
+    if(flag) out<<"true\n";
+    else out<<"false\n";
+    
+}
+
+bool Solve = [](){
+    std::ofstream out("user.out");
+    for (std::string s; std::getline(std::cin, s);) {
+        parse_input_and_solve(out, s);
+    }
+    out.flush();
+    exit(0);
+    return true;
+}();
+
+
+
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
