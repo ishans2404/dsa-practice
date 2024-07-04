28d0dfa98fc8334fabfa7b3f7f511b206fb27a44
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-07-04 12:46:42 +0530

    Time: 144 ms (99.77%), Space: 23.3 MB (99.77%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -1,3 +1,49 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+void parse_input_and_solve(std::ofstream& out, std::string& s) {
+    s.erase(s.begin());
+    s.erase(s.begin());
+    s.erase(s.begin());
+    s.pop_back();
+    string w;
+    vector<int> v;
+    istringstream iss(s);
+    int sum = 0;
+    while(getline(iss, w, ',')) {
+        int val = stoi(w);
+        if(val == 0) {
+            v.push_back(sum);
+            sum = 0;
+        }
+        else sum += val;
+    }
+
+    sum = v.size();
+    out << "[";
+    for(int i=0; i<sum-1; i++) out << v[i] << ",";
+    out << v[sum-1] << "]\n";
+}
+
+bool Solve = [](){
+    std::ofstream out("user.out");
+    for (std::string s; std::getline(std::cin, s);) {
+        parse_input_and_solve(out, s);
+    }
+    out.flush();
+    exit(0);
+    return true;
+}();
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -11,33 +57,19 @@
 class Solution {
 public:
     ListNode* mergeNodes(ListNode* head) {
-        vector<int> v;
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
         ListNode* curr = head->next;
         int sum = 0;
         while(curr) {
             if(curr->val == 0) {
-                v.push_back(sum);
+                tail->next = new ListNode(sum);
+                tail = tail->next;
                 sum = 0;
             }
             else sum += curr->val;
             curr = curr->next;
         }
-        sum = v.size();
-        int i = 0;
-        curr = head;
-        while(i < sum-1) {
-            curr->val = v[i++];
-            curr = curr->next;
-        }
-        curr->val = v[i];
-        ListNode* temp = curr->next;
-        
-        while(temp) {
-            ListNode* prev = temp;
-            temp = temp->next;
-            delete prev;
-        }
-        curr->next = nullptr;
-        return head;
+        return dummy.next;
     }
 };
