cdde0dab11dd02131f50be2734a29fac93d5ddb0
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-07-05 13:36:31 +0530

    Time: 70 ms (100.00%), Space: 25.7 MB (100.00%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,3 +1,71 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+void parse_input_and_solve(std::ofstream& out, std::string& s) {
+    s.erase(s.begin());
+    s.pop_back();
+    string w;
+    istringstream iss(s);
+    vector<int> nums;
+    while(getline(iss, w, ',')) nums.push_back(stoi(w));
+    
+    int prev = 0, curr = 1, nxt = 2;
+
+    int minDist = INT_MAX, maxDist = INT_MIN;
+    int firstCritical = -1, lastCritical = -1, prevCritical = -1;
+    int pos = 1;
+
+    string res;
+    int n = nums.size();
+    if(n < 3) {
+        out << "[-1,-1]\n";
+        goto lab;
+    }
+
+    while(nxt < n) {
+        if((nums[curr] > nums[prev] && nums[curr] > nums[nxt]) ||
+        (nums[curr] < nums[prev] && nums[curr] < nums[nxt])) {
+            if(firstCritical == -1) firstCritical = pos;
+            else minDist = min(minDist, pos - prevCritical);
+            prevCritical = pos;
+            lastCritical = pos;
+        }
+        prev = curr;
+        curr = nxt;
+        nxt++;
+        pos++;            
+    }
+    if(firstCritical == lastCritical) {
+        out << "[-1,-1]\n";
+        goto lab;
+    }
+
+    maxDist = lastCritical - firstCritical;
+    res = "[" + to_string(minDist) + "," + to_string(maxDist) + "]\n";
+    out << res;
+    lab:
+}
+
+bool Solve = [](){
+    std::ofstream out("user.out");
+    for (std::string s; std::getline(std::cin, s);) {
+        parse_input_and_solve(out, s);
+    }
+    out.flush();
+    exit(0);
+    return true;
+}();
+
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
