d505734d2b40d7f30ffc1e7e5aa853ade52df322
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-07-08 19:33:53 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (92.77%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,3 +1,36 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+void parse_input_and_solve(std::ofstream& out, std::string& s, std::string& t) {
+    int n = stoi(s);
+    int k = stoi(t);
+    int winner = 0;
+    for(int i = 1; i <= n; i++) {
+        winner = (winner + k) % i;
+    }
+    out << winner + 1 << endl;
+}
+
+bool Solve = []() {
+    std::ofstream out("user.out");
+    std::string s, t;
+    while (std::getline(std::cin, s) && std::getline(std::cin, t)) {
+        parse_input_and_solve(out, s, t);
+    }
+    out.flush();
+    exit(0);
+    return true;
+}();
+
+
+
 class Solution {
 public:
     int findTheWinner(int n, int k) {
