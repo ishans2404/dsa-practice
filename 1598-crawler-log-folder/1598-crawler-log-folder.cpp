25422df64eb42ba0df4e491585f8328139d1ae1c
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-07-10 14:58:44 +0530

    Time: 4 ms (67.13%), Space: 14.1 MB (41.82%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.cpp
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -1,41 +1,9 @@
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
-
-static const bool Booster = [](){
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return true;
-}();
-
-void parse_input_and_solve(std::ofstream& out, std::string& s) {
-    s.erase(s.begin());
-    s.pop_back();
-    string w;
-    istringstream iss(s);
-    int c = 0;
-    while(getline(iss, w, ',')) {
-        if(w == "\"./\"") continue;
-        else if(w == "\"../\"") c = (c>0)? c-1 : c;
-        else c++;
-    }
-    out << c << endl;
-}
-
-bool Solve = [](){
-    std::ofstream out("user.out");
-    for (std::string s; std::getline(std::cin, s);) {
-        parse_input_and_solve(out, s);
-    }
-    out.flush();
-    exit(0);
-    return true;
-}();
-
-
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
         int c = 0;
         for(auto& s : logs) {
             if(s == "./") continue;
