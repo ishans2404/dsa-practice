addf3a40a59195b249d909571627092e43dc48b2
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-07-11 14:57:30 +0530

    Time: 5 ms (13.35%), Space: 9.3 MB (18.51%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,3 +1,50 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+void parse_input_and_solve(std::ofstream& out, std::string& s) {
+    s.erase(s.begin());
+    s.pop_back();    
+    stack<char> st;
+    for(auto& ch : s) {
+        if(ch == ')') {
+            string revstr = "";
+            while(st.top() != '(') {
+                revstr += st.top();
+                st.pop();
+            }
+            if(!st.empty())  st.pop();
+            for(auto& c : revstr) st.push(c);
+        } 
+        else st.push(ch);
+    }
+ 
+    char ch = char(34);
+    string ans = "";
+    while(!st.empty()) {
+        ans = st.top() + ans;
+        st.pop();
+    }
+    out << ch << ans << ch << endl;
+}
+
+bool Solve = [](){
+    std::ofstream out("user.out");
+    for (std::string s; std::getline(std::cin, s);) {
+        parse_input_and_solve(out, s);
+    }
+    out.flush();
+    exit(0);
+    return true;
+}();
+
+
 class Solution {
 public:
     string reverseParentheses(string s) {
