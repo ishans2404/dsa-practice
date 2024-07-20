38c96a794dab9bb410c5157baeaba3a576cff449
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-07-20 20:07:19 +0530

    Time: 28 ms (93.45%), Space: 36.2 MB (6.09%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
@@ -5,17 +5,20 @@ public:
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
         
-        int n = rowSum.size(), m = colSum.size();
-        vector<vector<int>> grid(n, vector<int>(m, 0));
-        int i = 0, j = 0;
+        int i = rowSum.size()-1, j = colSum.size()-1;
+        vector<vector<int>> grid(i+1, vector<int>(j+1, 0));
 
-        while(i < n && j < m) {
-            int elem = min(rowSum[i], colSum[j]);
-            grid[i][j] = elem;
-            rowSum[i] -= elem;
-            colSum[j] -= elem;
-            i += (rowSum[i] == 0);
-            j += (colSum[j] == 0);
+        while(i>=0 && j >=0) {
+            if(rowSum[i] <= colSum[j]) {
+                grid[i][j] = rowSum[i];
+                colSum[j] -= rowSum[i];
+                i--;
+            }
+            else {
+                grid[i][j] = colSum[j];
+                rowSum[i] -= colSum[j];
+                j--;
+            }
         }
         return grid;
     }
