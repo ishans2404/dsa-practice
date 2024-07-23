8f6c485adad26014075d55510d5fb8b81f304cbc
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-07-23 21:55:09 +0530

    Time: 3 ms (89.91%), Space: 13.9 MB (98.16%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        int n = nums.size();
+        int freq[201] = {0};
+        for(int& i:nums) freq[i+100]++;
+
+        sort(nums.begin(), nums.end(), [&](int& x, int& y) {
+            return (freq[x+100] == freq[y+100]) ? x>y : freq[x+100] < freq[y+100];
+        });
+
+        return nums;
+    }
+};
