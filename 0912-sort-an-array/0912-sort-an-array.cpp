4ed755b8b43ec9a87aec095ed29b8bf1798ec393
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-07-25 21:38:52 +0530

    Time: 210 ms (50.77%), Space: 41.4 MB (100.00%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,4 +1,46 @@
 #include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+void parse_input_and_solve(std::ofstream& out, std::string& s) {
+    s.erase(s.begin());
+    s.pop_back();
+    map<int, int> m;
+    istringstream iss(s);
+    string w;
+    int n = 0, k = 0;
+    while(getline(iss, w, ',')) {
+        m[stoi(w)]++;
+        n++;
+    }
+    out << "[";
+    for(auto& it:m) {
+        for(int i=0; i<it.second; i++) {
+            k++;
+            if(k != n) out << it.first << ",";
+            else out << it.first;
+        }
+    }
+    out << "]\n";
+}
+
+bool Solve = [](){
+    std::ofstream out("user.out");
+    for (std::string s; std::getline(std::cin, s);) {
+        parse_input_and_solve(out, s);
+    }
+    out.flush();
+    exit(0);
+    return true;
+}();
+
 
 class Solution {
 public:
