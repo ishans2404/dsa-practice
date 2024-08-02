7332908b33ec95aabd8c6940900baa16b2c36d56
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-08-02 21:53:00 +0530

    Time: 57 ms (94.11%), Space: 19.7 MB (100.00%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -13,12 +13,63 @@
 //             if(nums[i-wndw] == 0) temp--;
 //             if(nums[i%n] == 0) temp++;
 //             res = min(res, temp);
-//             cout<<res<<" ";
 //         }
-//         cout<<endl;
 //         return res;
 //     }
 // };
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+int helper(vector<int>& nums) {
+    int n = nums.size();
+    int ones = 0;
+    for(auto& i : nums) ones += i;
+    if(ones == 0 || ones == n) return 0;
+    
+    int cnt = 0; // 1st window
+    for(int i = 0; i < ones; i++) cnt += nums[i];
+    int res = cnt;
+    for(int i = 0; i < n; i++) {
+        cnt -= nums[i];
+        cnt += nums[(i + ones) % n];
+        res = max(res, cnt);
+    }
+    
+    return ones - res;
+}
+
+void parse_input_and_solve(std::ofstream& out, std::string& s) {
+    s.erase(s.begin());
+    s.pop_back();
+    string w;
+    istringstream iss(s);
+    vector<int> nums;
+    while(getline(iss, w, ',')) {
+        nums.push_back(stoi(w));
+    }
+
+    out << helper(nums) << endl;
+}
+
+bool Solve = [](){
+    std::ofstream out("user.out");
+    for (std::string s; std::getline(std::cin, s);) {
+        parse_input_and_solve(out, s);
+    }
+    out.flush();
+    exit(0);
+    return true;
+}();
+
+
 
 class Solution {
 public:
