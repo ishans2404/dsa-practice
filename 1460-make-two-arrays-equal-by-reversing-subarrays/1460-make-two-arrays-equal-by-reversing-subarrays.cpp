5f046c62a6099ed379783bf91e5eaf3c87cc4016
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-08-03 21:52:07 +0530

    Time: 12 ms (34.04%), Space: 18.8 MB (28.14%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,12 +1,40 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        vector<vector<int>> v(1001, vector<int>(2, 0));
-        for(auto& i : target) v[i][0]++;
-        for(auto& i : arr) v[i][1]++;
-        for(int i=1; i<1001; i++) {
-            if(v[i][0] != v[i][1]) return false;
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+
+        target = countSort(target);
+        arr = countSort(arr);
+        int n = target.size();
+        for(int i=0; i<n; i++) {
+            if(target[i] != arr[i]) return false;
         }
         return true;
+    }
+private:
+    vector<int> countSort(vector<int>& inputArray) {
+        int N = inputArray.size();
+        int M = 0;    
+        for (int i = 0; i < N; i++) M = max(M, inputArray[i]);
+    
+        vector<int> countArray(M + 1, 0);
+
+        for (int i = 0; i < N; i++)
+            countArray[inputArray[i]]++;
+
+        for (int i = 1; i <= M; i++)
+            countArray[i] += countArray[i - 1];
+    
+        // Creating outputArray[] from countArray[] array
+        vector<int> outputArray(N);
+    
+        for (int i = N - 1; i >= 0; i--) {
+            outputArray[countArray[inputArray[i]] - 1] = inputArray[i];
+            countArray[inputArray[i]]--;
+        }
+    
+        return outputArray;
     }
 };
