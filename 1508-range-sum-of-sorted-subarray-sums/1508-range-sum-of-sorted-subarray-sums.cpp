fdd4ca77d3a29e5fbd8a4605768fadd1bb52a7f7
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-08-04 22:07:44 +0530

    Time: 116 ms (10.79%), Space: 27.6 MB (17.08%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
-    int rangeSum(vector<int>& nums, int n, int l, int r) {
-        vector<int> v;
-        const int mod = 1e9 + 7;
-        for(int i=0; i<n; i++) {
-            int pre = 0;
-            for(int j=i; j<n; j++) {
-                pre += nums[j];
-                v.push_back(pre);
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+        
+        const int MOD = 1000000007;
+        
+        vector<int> prefix(n + 1, 0);
+        for (int i = 0; i < n; ++i) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+        
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                minHeap.push(prefix[i] - prefix[j]);
             }
         }
-        sort(v.begin(), v.end());
-        long long res = 0;
-        for(int i=l-1; i<=r-1; i++) res += v[i];
-        res %= mod;
-
-        return res;
+        
+        long long result = 0;
+        for (int i = 1; i <= right; ++i) {
+            int sum_value = minHeap.top();
+            minHeap.pop();
+            if (i >= left) {
+                result = (result + sum_value) % MOD;
+            }
+        }
+        
+        return result;
     }
 };
