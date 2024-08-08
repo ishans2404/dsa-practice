ee5ba3cd6bf6c1e0f125629924b40aea6db9b459
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-08-08 21:04:00 +0530

    Time: 87 ms (37.89%), Space: 17.8 MB (38.67%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
@@ -1,20 +1,20 @@
 class Solution:
     def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
-        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]  # left, down, right, up
-        res = []  # store valid pair
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # right, down, left, up
+        result = []
         steps = 1
-        direction = 0
-        r = rStart
-        c = cStart
-        total = rows * cols
-        while len(res) < total:
-            for a in range(2):
-                rnew, cnew = directions[direction]
-                for b in range(steps):
-                    if (0 <= r < rows and 0 <= c < cols):
-                        res.append([r, c])
-                    r += rnew
-                    c += cnew
-                direction = (direction + 1) % 4
+        d = 0
+        r, c = rStart, cStart
+        
+        while True:
+            for _ in range(2):  # Two times per layer
+                for _ in range(steps):
+                    if 0 <= r < rows and 0 <= c < cols:
+                        result.append([r, c])
+                        if len(result) == rows * cols: return result
+                    r += directions[d][0]
+                    c += directions[d][1]
+                d = (d + 1) % 4
             steps += 1
-        return res
+        
+        return result
