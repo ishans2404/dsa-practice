813ef64a9c893eb24c0b05e0822d8b1e96d361de
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-08-08 21:14:41 +0530

    Time: 70 ms (96.09%), Space: 17.9 MB (38.67%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
@@ -1,26 +1,27 @@
 class Solution:
     def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
-        i = rStart
-        j = cStart
+        i, j = rStart, cStart
         cRow, cCol = 0, 0
         row, col = 1, 1
         required = rows * cols
 
         res = [[i, j]]
 
+        def valid(i, j):
+            if j > -1 and j < cols and i > -1 and i < rows:
+                res.append([i, j])
+
         while len(res) < required:
             while  cCol < col:
                 j += 1
-                if j > -1 and j < cols and i > -1 and i < rows:
-                    res.append([i, j])
+                valid(i, j)
                 cCol += 1
 
             if len(res) >= required: break
             
             while cRow < row:
                 i += 1
-                if j > -1 and j < cols and i > -1 and i < rows:
-                    res.append([i, j])
+                valid(i, j)
                 cRow += 1
 
             if len(res) >= required: break
@@ -31,16 +32,14 @@ class Solution:
 
             while cCol < col:
                 j -= 1
-                if j > -1 and j < cols and i > -1 and i < rows:
-                    res.append([i, j])
+                valid(i, j)
                 cCol += 1
 
             if len(res) >= required: break
             
             while cRow < row:
                 i -= 1
-                if j > -1 and j < cols and i > -1 and i < rows:
-                    res.append([i, j])
+                valid(i, j)
                 cRow += 1
 
             if len(res) >= required: break
