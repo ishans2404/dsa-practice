a6d2c0df228e3a12369fb42c89ba54765e955b4e
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-08-26 02:31:33 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (45.11%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    private void find(TreeNode root, List<Integer> ans) {
+        if (root == null) return;
+        find(root.left, ans);
+        find(root.right, ans);
+        ans.add(root.val);
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        find(root, ans);
+        return ans;
+    }
+}
