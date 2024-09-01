e182776cf30166c23b25c4b0f81a1a2608d722e1
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-09-01 21:13:22 +0530

    Time: 2 ms (100.00%), Space: 54.9 MB (98.94%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
@@ -1,13 +1,10 @@
 class Solution {
     public int[][] construct2DArray(int[] og, int m, int n) {
-        int len = og.length;
-        if(m*n != len) return new int[0][0];
-        int[][] arr = new int[m][n];
-        int k = 0;
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                arr[i][j] = og[k++];
-            }
+        if(m*n != og.length) return new int[0][0];
+        int[][] arr = new int[m][];
+
+        for(int i=0; i<m; i++){
+           arr[i] = Arrays.copyOfRange(og, i*n, i*n+n);
         }
         return arr;
     }
