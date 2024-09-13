7fd0602021e3e62a4cdd0834541eaa7f6c5ebf6d
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-09-13 23:46:22 +0530

    Time: 2 ms (100.00%), Space: 58.7 MB (14.50%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
@@ -3,17 +3,15 @@ class Solution {
         int n = queries.length;
         int m = arr.length;
         int[] nums = new int[n];
-        int[] pre = new int[m];
-        pre[0] = arr[0];
         for(int i=1; i<m; i++) {
-            pre[i] = pre[i-1] ^ arr[i];
+            arr[i] ^= arr[i-1];
         }
 
         for(int i=0; i<n; i++) {
             int l = queries[i][0];
             int r = queries[i][1];
-            if(l == 0) nums[i] = pre[r];
-            else nums[i] = pre[r] ^ pre[l-1];
+            if(l == 0) nums[i] = arr[r];
+            else nums[i] = arr[r] ^ arr[l-1];
         }
         return nums;
     }
