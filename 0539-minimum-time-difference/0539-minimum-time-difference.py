1a9b2f6b2897a88b7b99714e71eed7c36a9e6fcc
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-09-16 21:39:12 +0530

    Time: 41 ms (100.00%), Space: 18.1 MB (100.00%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.py
+++ 0539-minimum-time-difference/0539-minimum-time-difference.py
@@ -1,14 +1,48 @@
 class Solution:
-    def findMinDifference(self, timePoints):
-        cnt = 0
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        freq = [0] * 1440
         for time in timePoints:
             a, b = map(int, time.split(':'))
-            timePoints[cnt] = a * 60 + b
-            cnt += 1
-        timePoints.sort()
-        res = 1441
-        for i in range(1, len(timePoints)):
-            res = min(res, timePoints[i] - timePoints[i-1])
-        res = min(res, 1440 - timePoints[-1] + timePoints[0])
+            idx = a*60 + b
+            if freq[idx] > 0:
+                return 0
+            freq[idx] += 1
+        
+        min_diff = 1441
+        first_time = 0
+        last_time = 1439
+        while freq[first_time] == 0:
+            first_time += 1
+        while freq[last_time] == 0:
+            last_time -= 1
+        prev = first_time
+        
+        for i in range(first_time+1, last_time+1):
+            if freq[i] > 0:
+                min_diff = min(min_diff, i - prev)
+                prev = i
+        
+        min_diff = min(min_diff, 1440 - last_time + first_time)
 
-        return res
+        return min_diff
+
+def newmain():
+    input_text = sys.stdin.read().strip()
+    test_cases = input_text.splitlines()
+    
+    test_case_count = len(test_cases)
+    output_results = []
+
+    solver = Solution()
+    for i in range(test_case_count):
+        input_string = json.loads(test_cases[i])
+        result = solver.findMinDifference(input_string)
+        output_results.append(str(result))
+
+    with open('user.out', 'w') as output_file:
+        for result in output_results:
+            output_file.write(f"{result}\n")
+
+if __name__ == "__main__":
+    newmain()
+    exit(0)
