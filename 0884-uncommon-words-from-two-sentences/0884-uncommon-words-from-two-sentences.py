cdb4f3c623eb73f5068302397d9317c494aa6159
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-09-17 19:41:32 +0530

    Time: 32 ms (78.21%), Space: 16.7 MB (5.76%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
@@ -7,25 +7,3 @@ class Solution:
         set2 = [w2 for w2 in count2.keys() if count1[w2] <= 1 and count2[w2] <= 1]
 
         return list(set(set1) - set(set2)) + list(set(set2) - set(set1))
-
-def newmain():
-    input_text = sys.stdin.read().strip()
-    test_cases = input_text.splitlines()
-    
-    test_case_count = len(test_cases) // 2
-    output_results = []
-
-    solver = Solution()
-    for i in range(test_case_count):
-        s1 = json.loads(test_cases[2*i])
-        s2 = json.loads(test_cases[2*i+1])
-        result = solver.uncommonFromSentences(s1, s2)
-        output_results.append(result)
-
-    with open('user.out', 'w') as output_file:
-        for result in output_results:
-            output_file.write(f"{json.dumps(result)}\n")
-
-if __name__ == "__main__":
-    newmain()
-    exit(0)
