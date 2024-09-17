15873c2c237dda1b5b41472c9c519346a68fa8c9
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-09-17 19:39:49 +0530

    Time: 42 ms (13.59%), Space: 16.6 MB (44.21%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
@@ -1,11 +1,28 @@
-from collections import Counter
+
 class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
-        a = list(map(str, s1.split(' ')))
-        b = list(map(str, s2.split(' ')))
-        dic = Counter(a + b)
-        a = []
-        for elem in dic.keys():
-            if dic[elem] == 1:
-                a.append(elem)
-        return a
+        word_count = Counter(s1.split()) + Counter(s2.split())
+        res = [word for word, count in word_count.items() if count == 1]
+        return res
+
+def newmain():
+    input_text = sys.stdin.read().strip()
+    test_cases = input_text.splitlines()
+    
+    test_case_count = len(test_cases) // 2
+    output_results = []
+
+    solver = Solution()
+    for i in range(test_case_count):
+        s1 = json.loads(test_cases[2*i])
+        s2 = json.loads(test_cases[2*i+1])
+        result = solver.uncommonFromSentences(s1, s2)
+        output_results.append(result)
+
+    with open('user.out', 'w') as output_file:
+        for result in output_results:
+            output_file.write(f"{json.dumps(result)}\n")
+
+if __name__ == "__main__":
+    newmain()
+    exit(0)
