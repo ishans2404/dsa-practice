f47494cd2bd81acdeb7814ba6341ca4065a2c2f9
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-09-22 00:10:44 +0530

    Time: 57 ms (98.56%), Space: 22.4 MB (86.72%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -1,5 +1,9 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        res = list(range(1, n+1))
-        res.sort(key=lambda a : str(a))
-        return res
+        return sorted(list(range(1, n+1)), key=lambda x:str(x))
+
+with open("user.out", "w") as f:
+    inputs = map(loads, stdin)
+    for nums in inputs:
+        print(str(Solution().lexicalOrder(nums)).replace(" ", ""), file=f)
+exit(0)
