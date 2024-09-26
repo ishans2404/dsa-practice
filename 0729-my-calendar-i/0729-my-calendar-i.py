6e5f99433a35f24bd854bffa6407d529fbf9ffb1
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-09-26 23:52:14 +0530

    Time: 147 ms (97.72%), Space: 17.3 MB (65.25%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,35 +1,21 @@
-class BST:
-    def __init__(self, start, end):
-        self.left = None
-        self.right = None
-        self.start = start
-        self.end = end
-
-    def insert(self, start, end):
-        curr = self
-        while True:
-            if end <= curr.start:
-                if not curr.left:
-                    curr.left = BST(start, end)
-                    return True
-                curr = curr.left
-            elif curr.end <= start:
-                if not curr.right:
-                    curr.right = BST(start, end)
-                    return True
-                curr = curr.right
-            else:
-                return False
+import bisect
 
 class MyCalendar:
     def __init__(self):
-        self.root = None        
+        self.intervals = []
 
     def book(self, start: int, end: int) -> bool:
-        if not self.root:
-            self.root = BST(start, end)
-            return True
-        return self.root.insert(start, end)
+        pos = bisect.bisect_left(self.intervals, (start, end))  # position for new interval
+        
+        # check overlap
+        if pos > 0 and self.intervals[pos - 1][1] > start:
+            return False
+        if pos < len(self.intervals) and self.intervals[pos][0] < end:
+            return False
+        
+        # insert
+        self.intervals.insert(pos, (start, end))
+        return True
 
 
 # Your MyCalendar object will be instantiated and called as such:
