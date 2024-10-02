e5361abb2cf01ebf35bf154a17c3f20df3c774ba
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-10-02 21:33:03 +0530

    Time: 274 ms (13.65%), Space: 28.3 MB (100.00%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -1,19 +1,17 @@
 from heapq import heapify, heappush, heappop
 class Solution:
-    def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        if not arr:
+    def arrayRankTransform(self, nums: List[int]) -> List[int]:
+        if not nums:
             return []
-        res = arr.copy()
-        n = len(res)
-        heapify(arr)
-        dic = {}
-        cnt = 1
+        n = len(nums)
+        idx = list(range(n))
+        idx.sort(key=lambda x: nums[x])
+        prev = int(-1e9 - 1)
+        rank = 0
         for i in range(n):
-            elem = heappop(arr)
-            if elem in dic:
-                continue
-            dic[elem] = cnt
-            cnt += 1
-        for i in range(n):
-            res[i] = dic[res[i]]
-        return res
+            index = idx[i]
+            if nums[index] != prev:
+                rank += 1
+            prev = nums[index]
+            nums[index] = rank
+        return nums
