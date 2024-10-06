8ceea308db2014687271a2349baa55687c08d74a
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2024-10-07 01:16:58 +0530

    Time: 34 ms (61.88%), Space: 16.5 MB (47.19%) - LeetHub

--- 1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
@@ -16,3 +16,37 @@ class Solution:
                 break
             suf += 1
         return suf + pre > m - 1
+
+def newmain():
+    input_data = sys.stdin.read().strip()
+    lines = input_data.splitlines()
+    
+    num_test_cases = len(lines) // 2
+    results = []
+
+    for i in range(num_test_cases):
+        s1 = lines[2*i][1:-1].split()
+        s2 = lines[2*i+1][1:-1].split()
+        if len(s2) > len(s1):
+            s1, s2 = s2, s1
+        n, m = len(s1), len(s2)
+        pre = 0
+        suf = 0
+        while pre < n and pre < m:
+            if s1[pre] != s2[pre]:
+                break
+            pre += 1
+        while suf < n and suf < m:
+            if s1[n - 1 - suf] != s2[m - 1 - suf]:
+                break
+            suf += 1
+        result = suf + pre > m - 1
+        results.append(json.dumps(result, separators=(',', ':')))
+
+    with open('user.out', 'w') as f:
+        for result in results:
+            f.write(f"{result}\n")
+
+if __name__ == "__main__":
+    newmain()
+    exit(0)
