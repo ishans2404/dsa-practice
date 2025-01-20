0363438f0120b46a9ca7ef8e9580c3cdcd0c1261
Ishan Singh <127775716+ishans2404@users.noreply.github.com>
2025-01-20 20:21:41 +0530

    Time: 96 ms, Memory: 43.8 MB - LeetHub

---
+++ 0855-30-2661-first-completely-painted-row-or-column/0855-30-2661-first-completely-painted-row-or-column.py
@@ -0,0 +1,16 @@
+class Solution:
+    def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+        m, n, sz = len(mat), len(mat[0]), len(arr)
+        lookup = [None] * (sz + 1)
+        for r in range(m):
+            for c in range(n):
+                lookup[mat[r][c]] = (r, c)
+        row_fill = [0] * m
+        col_fill = [0] * n
+        for i in range(len(arr)):
+            r, c = lookup[arr[i]]
+            row_fill[r] += 1
+            col_fill[c] += 1
+            if row_fill[r] == n or col_fill[c] == m:
+                return i
+        return -1
